11827ee7c919ed4a02dbd12bcbd6a901f98360db
Miguel <miguelmuscat93@gmail.com>
2020-12-01 20:47:10 +0100

    Added node visitor that finds declarations

---
+++ src/Visitors/DeclarationFinder.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dhii\PhpCage\Visitors;
+
+use Dhii\PhpCage\Context\Context;
+use Dhii\PhpCage\Context\Symbol;
+use PhpParser\Node;
+
+/**
+ * Analyzes an AST and extracts declarations.
+ *
+ * @since [*next-version*]
+ */
+class DeclarationFinder extends AbstractNsAwareVisitor
+{
+    /**
+     * @since [*next-version*]
+     */
+    protected Context $ctx;
+
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param Context $context The context.
+     */
+    public function __construct(Context $context)
+    {
+        $this->ctx = $context;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @since [*next-version*]
+     */
+    public function beforeTraverse(array $nodes): void
+    {
+        parent::beforeTraverse($nodes);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @since [*next-version*]
+     */
+    public function leaveNode(Node $node)
+    {
+        if (($node instanceof Node\Stmt\ClassLike || $node instanceof Node\Stmt\Function_) && $node->name !== null) {
+            $fqn = $this->resolveQualifiedName($node->name);
+            $symbol = new Symbol($node);
+
+            $this->ctx->addSymbol($fqn, $symbol);
+        }
+    }
+}
