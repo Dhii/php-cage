90d77f8590c73e6df241bc42a65dc840d8c23163
Miguel <miguelmuscat93@gmail.com>
2020-12-01 21:09:22 +0100

    Added transform list

---
+++ src/Transforms/TransformList.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dhii\PhpCage\Transforms;
+
+use Dhii\PhpCage\Context;
+use Dhii\PhpCage\Transform;
+
+/**
+ * A transform implementation that applies a list of other transformations.
+ *
+ * @since [*next-version*]
+ */
+class TransformList implements Transform
+{
+    /**
+     * @since [*next-version*]
+     *
+     * @var Transform[]
+     */
+    protected array $transforms;
+
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param Transform[] $transforms The transformations to apply.
+     */
+    public function __construct(array $transforms)
+    {
+        $this->transforms = $transforms;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @since [*next-version*]
+     */
+    public function apply(Context $context): void
+    {
+        foreach ($this->transforms as $transform) {
+            $transform->apply($context);
+        }
+    }
+}
