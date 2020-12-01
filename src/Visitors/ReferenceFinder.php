1ca79958a49d7381b7627c5cdd817c7e05770776
Miguel <miguelmuscat93@gmail.com>
2020-12-01 20:47:21 +0100

    Added node visitor that finds references

---
+++ src/Visitors/ReferenceFinder.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dhii\PhpCage\Visitors;
+
+use Dhii\PhpCage\Context\Context;
+use PhpParser\Node;
+
+/**
+ * A node visitor that detects and records references to symbols in a given {@link Context} instance.
+ *
+ * @since [*next-version*]
+ */
+class ReferenceFinder extends AbstractNsAwareVisitor
+{
+    /**
+     * @since [*next-version*]
+     */
+    protected Context $ctx;
+
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param Context $ctx The context.
+     */
+    public function __construct(Context $ctx)
+    {
+        $this->ctx = $ctx;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @since [*next-version*]
+     */
+    public function beforeTraverse(array $nodes)
+    {
+        parent::beforeTraverse($nodes);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @since [*next-version*]
+     */
+    public function enterNode(Node $node)
+    {
+        parent::enterNode($node);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @since [*next-version*]
+     */
+    public function leaveNode(Node $node)
+    {
+        if ($node instanceof Node\Name || $node instanceof Node\Identifier) {
+            $fqn = $this->resolveQualifiedName($node);
+
+            $symbol = $this->ctx->getSymbol($fqn);
+
+            if ($symbol !== null && $symbol->decl !== null) {
+                $symbol->refs[] = $node;
+            }
+        } elseif ($node instanceof Node\Stmt\Namespace_) {
+            $this->ctx->getNamespace($node->name->toString())->addRef($node->name);
+        }
+    }
+}
