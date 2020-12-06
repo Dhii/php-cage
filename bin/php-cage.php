11b3a60ebebe7866b196d73693a865b4939be827
Miguel <miguelmuscat93@gmail.com>
2020-12-06 17:19:27 +0100

    Added console application

---
+++ bin/php-cage.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Dhii\PhpCage\Cli\CageCommand;
+use Symfony\Component\Console\Application;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = new Application();
+$app->add(new CageCommand());
+$app->setDefaultCommand('cage', true);
+
+$app->run();
