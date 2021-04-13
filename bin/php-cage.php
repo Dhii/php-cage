fe451285e113e1e8b75d0fefac93a118b69e6d36
Anton Ukhanev <xedin.unknown@gmail.com>
2021-04-13 11:34:07 +0200

    Fix autoload path

--- bin/php-cage.php
+++ bin/php-cage.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Dhii\PhpCage\Cli\CageCommand;
 use Symfony\Component\Console\Application;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application();
 $app->add(new CageCommand());
